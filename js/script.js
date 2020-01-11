94eb79b2c80072aa0040bb0ac3e6252a30b28f5a
unknown <zakir_hockey@mail.ru>
2020-01-11 17:46:59 +0300

    доработка

---
+++ js/script.js
@@ -0,0 +1,63 @@
+
+function interes(){
+    var boxvive = document.getElementById('newinteresbox');
+    boxvive.style.display = 'block';
+}
+function intdel() {
+    var parent = document.getElementById('');
+    var deleteint = document.getElementsByClassName('user-info__interes');
+    parent.removeChild(deleteint);
+}
+function intpush() {
+    var interess = document.getElementById('newinteres');
+    var newint = interess.value;
+    var div = document.getElementById('box');
+    if(newint != null && newint != undefined && newint.length != 0) {
+        var a = document.createElement('a');
+        a.innerHTML = newint;
+        a.href = '#';
+        a.className = 'user-info__interes';
+        div.insertBefore(a, div.firstElementChild);
+        var boxvive = document.getElementById('newinteresbox');
+        boxvive.style.display = 'none';
+        interess.value = '';
+    }
+    a.addEventListener('click', function () {
+        div.removeChild(a);
+    }) 
+}
+$(document).ready(function(){ 
+    $('.user-info__interes').click( function(){
+        $(this).remove();
+    })
+})
+
+function interesoff(){
+    var boxvive = document.getElementById('newinteresbox');
+    boxvive.style.display = 'none';
+}
+
+function func() {
+    var elem = document.getElementById('test');
+    var elem2 = document.getElementById('test1');
+    elem.style.color = '#000000';
+    elem.style.background = '#ffffff';
+    elem2.style.color = '#ffffff';
+    elem2.style.background = '#1e69ac';
+    var user = document.getElementById('user');
+    user.style.display = 'none';
+    var friends = document.getElementById('friends');
+    friends.style.display = 'block';
+}
+function func1() {
+    var elem = document.getElementById('test1');
+    var elem2 = document.getElementById('test');
+    elem.style.color = '#000000';
+    elem.style.background = '#ffffff';
+    elem2.style.color = '#ffffff';
+    elem2.style.background = '#1e69ac';
+    var user = document.getElementById('user');
+    user.style.display = 'grid';
+    var friends = document.getElementById('friends');
+    friends.style.display = 'none';
+}
